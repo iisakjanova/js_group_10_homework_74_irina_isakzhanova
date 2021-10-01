4f4be55a9088bd35eecfd1f7b250c0a2623077d4
Irina Isakzhanova <irina.isakjanova@gmail.com>
2021-10-01 21:36:40 +0600

    Implemented GET request processing

--- app/messages.js
+++ app/messages.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 
+const path = "./messages";
 const router = express.Router();
 
 router.post('/', (req, res) => {
@@ -21,6 +22,36 @@ router.post('/', (req, res) => {
   });
 
   res.send(body);
+});
+
+router.get('/', (req, res) => {
+  (async () => {
+    let listDir = [];
+
+    try {
+      const files = await fs.promises.readdir(path);
+      files.forEach(file => {
+        listDir.push(path + '/' + file);
+      });
+
+    } catch (e) {
+      console.error(e);
+    }
+
+    const lastFiveDirs = listDir.slice(-5);
+
+    let contents;
+
+    try {
+      const lastFiveDirsPromises = lastFiveDirs.map(dir => fs.promises.readFile(dir, 'utf8'));
+      contents = await Promise.all(lastFiveDirsPromises);
+    } catch (e) {
+      console.error(e);
+    }
+
+    const data = contents.map(content => JSON.parse(content));
+    res.send(data);
+  })();
 });
 
 module.exports = router;
